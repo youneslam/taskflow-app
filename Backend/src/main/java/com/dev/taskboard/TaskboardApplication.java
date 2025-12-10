0bb784286f07ee20439864015a971bf0a2d84d8e
youneslam <younes.lamnouar03@gmail.com>
2025-12-10 20:26:39 +0100

    chore: project structure initialization

---
+++ Backend/src/main/java/com/dev/taskboard/TaskboardApplication.java
@@ -0,0 +1,13 @@
+package com.dev.taskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskboardApplication.class, args);
+	}
+
+}
